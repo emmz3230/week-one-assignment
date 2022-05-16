50c422a5f8b37fbbe387b1f98157b1abfd6e48a7
emmz3230 <emmz3230@gmail.com>
2022-05-16 06:54:07 -0700

    week one assignment code

---
+++ datatypes.js
@@ -0,0 +1,42 @@
+// create datatypes.jse and nuse the javascript 
+// typeof operator to check different data types.
+// check the data type
+
+var figure = "0";
+var number = 3.50;
+var alpha = ["ade"]
+console.log(typeof(figure));
+console.log(typeof(number));
+console.log(typeof(alpha));
+
+// declare four variable without assigning values
+var empty;
+var pull;
+var oush;
+var out;
+
+// declare four variable with assigned values
+var one = 1;
+var two = "2";
+var three = ["3"];
+var four = 4;
+
+// declare  variables to store first name 
+// marital status,country and age in multiple lines 
+
+var firstaName = "Emmanuel Samuel";
+var maritalStatus = "single";
+var country = "Nigeria";
+var age = "20";
+
+// declare  variables to store first name 
+// marital status,country and age in single  lines
+var information =["firstName:Emmanuel Samuel", "maritalStatus:single",
+"country: Nigeria", "Age:20"]
+
+// declare two variable myAge and assign 
+// them initial values and log to the browser
+var myAge ="20";
+var yourAge ="27";
+console.log(myAge);
+console.log(yourAge);
